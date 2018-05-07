f6701bebac1472b93197624f63293d374ccdcf67
Kevin Boyd <kevin.boyd@uconn.edu>
2018-05-07 16:27:35 -0400

    more gitignore stuff, to ignore everything starting with test, prepped for more testing added

--- memcurv/tests/gromacs_test.sh
+++ memcurv/tests/gromacs_test.sh
@@ -3,18 +3,18 @@
 # generate a system and try to grompp it with topology and index 
 folder="gromacs_test_files"
 
-rm $folder/*.tpr $folder/*.gro
+rm $folder/*.tpr $folder/*.gro $folder/test*
 
-python3 ../main.py -f asymm.pdb -s sphere -z 10.5 -o $folder/start.pdb -p $folder/start.top -n $folder/start.ndx -g r_sphere:100
+python3 ../main.py -f asymm.pdb -s sphere -z 10.5 -o $folder/test_start.pdb -p $folder/test_start.top -n $folder/test_start.ndx -g r_sphere:100
 
-echo '#include "toppar/martini_v2.0_ions.itp"'              | cat - $folder/start.top > temp && mv temp $folder/start.top
-echo '#include "toppar/martini_v2.0_lipids_all_201506.itp"' | cat - $folder/start.top > temp && mv temp $folder/start.top
-echo '#include "toppar/martini_v2.2.itp"'                   | cat - $folder/start.top > temp && mv temp $folder/start.top
+echo '#include "toppar/martini_v2.0_ions.itp"'              | cat - $folder/test_start.top > temp && mv temp $folder/test_start.top
+echo '#include "toppar/martini_v2.0_lipids_all_201506.itp"' | cat - $folder/test_start.top > temp && mv temp $folder/test_start.top
+echo '#include "toppar/martini_v2.2.itp"'                   | cat - $folder/test_start.top > temp && mv temp $folder/test_start.top
 
 
-gmx grompp -f $folder/step6.0_equilibration.mdp -p $folder/start.top -n $folder/start.ndx -c $folder/start.pdb -o $folder/test_minimization.tpr 
+gmx grompp -f $folder/step6.0_equilibration.mdp -p $folder/test_start.top -n $folder/test_start.ndx -c $folder/test_start.pdb -o $folder/test_minimization.tpr 
 gmx mdrun -deffnm $folder/test_minimization -nsteps 40 -v
-gmx grompp -f $folder/step7_production.mdp -p $folder/start.top -n $folder/start.ndx -c $folder/test_minimization.gro -o $folder/grompp_production.tpr
+gmx grompp -f $folder/step7_production.mdp -p $folder/test_start.top -n $folder/test_start.ndx -c $folder/test_minimization.gro -o $folder/test_grompp_production.tpr
 
 
 
